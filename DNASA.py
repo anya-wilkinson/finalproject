750c6fbb03942736b9324c9afd839c307f8e8f6a
anya-wilkinson <tup22307@temple.edu>
2022-11-07 10:04:54 -0500

    Create DNASA.py
    what i've done in idle in lab (11/7) and trying to upload it to github

---
+++ DNASA.py
@@ -0,0 +1,35 @@
+#Anya Wilkinson
+
+testsequence = "ACGTACGTACGT"
+
+aminoacids = {}
+nucleotides = ['A', 'C', 'G', 'T']
+nucleotidecount = {'A':0, 'C':0, 'G':0, 'T':0}
+compliment = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+
+def basicanalysis(sequence):
+    #length
+    length = len(sequence)
+    print("BP count:", length, "nucleotides")
+    #nucleotide count
+    for char in sequence:
+        if char in nucleotides:
+            nucleotidecount[char] += 1
+    print("Nucleotide count:", nucleotidecount)
+    #GC%
+    G = nucleotidecount.get('G')
+    C = nucleotidecount.get('C')
+    GC = G + C
+    proportion = (GC / length)*100
+    print("GC%:", proportion, "%")
+    
+def complimentarysequence(sequence):
+    #give complementary DNA sequence
+    pass    
+    
+def translation(sequence):
+    #dna to amino acid
+    pass
+
+
+basicanalysis("AGGTGTTGGATTCAAAGCTGGTGTCAAGGATTACCGATTGACCTATTACACCCCCGAATACAAGACCAAAGATACCGACATCTTGGCAGCCTTCCGGATGACCCCACAACCCGGGGTACCAGCTGAGGAAGCCGGAGCTGCGGTAGCTGCGGAATCCTCCACGGGTA")
