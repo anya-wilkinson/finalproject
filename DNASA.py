d6a90e49221a0cfe9c295f96e5f067fa550ab2ef
anya-wilkinson <tup22307@temple.edu>
2022-11-13 19:53:29 -0500

    Update DNASA.py
    added function that makes the complimentary rna sequence from a given template dna seq

--- DNASA.py
+++ DNASA.py
@@ -39,6 +39,22 @@ def complimentarysequence(sequence):
             newseq = newseq + 'G'
     print(newseq)
     return newseq    
+
+def rnatemplate(sequence):
+    #assuming the sequence inputted is the template strand, return the corresponding RNA sequence
+    #same as complimentarysequence but uses uracil instead of thymine
+    newseq = ""
+    for char in sequence:
+        if char == 'A':
+            newseq = newseq + 'U'
+        if char == 'T':
+            newseq = newseq + 'A'
+        if char == 'G':
+            newseq = newseq + 'C'
+        if char == 'C':
+            newseq = newseq + 'G'
+    print(newseq)
+    return newseq
     
 def translation(sequence):
     #dna to amino acid
@@ -47,4 +63,4 @@ def translation(sequence):
 
 #basicanalysis("AGGTGTTGGATTCAAAGCTGGTGTCAAGGATTACCGATTGACCTATTACACCCCCGAATACAAGACCAAAGATACCGACATCTTGGCAGCCTTCCGGATGACCCCACAACCCGGGGTACCAGCTGAGGAAGCCGGAGCTGCGGTAGCTGCGGAATCCTCCACGGGTA")
 #testing changes to see how github works
-complimentarysequence("AGAGCTCT")
+rnatemplate("AGAGCTCT")
