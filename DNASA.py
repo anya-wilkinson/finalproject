cc0a4a10d5da34b0314ef80691e783267105b9ea
anya-wilkinson <tup22307@temple.edu>
2022-12-02 16:13:25 -0500

    working with files update
    updates on making the file reading work and learning how to write files

--- DNASA.py
+++ DNASA.py
@@ -5,7 +5,6 @@ testsequence = "ACGTACGTACGT"
 aminoacids = {"UUU":"Phenylalanine", "UUC":"Phenylalanine", "UUA":"Leucine", "UUG":"Leucine", "UCU":"Serine", "UCC":"Serine", "UCA":"Serine", "UCG":"Serine", "UAU":"Tyrosine", "UAC":"Tyrosine", 'UAA':'STOP', 'UAG':'STOP', 'UGU':'Cysteine', 'UGC':'Cysteine','UGA':'STOP', 'UGG':'Tryptophan', 'CUU':'Leucine', 'CUC':'Leucine', 'CUA':'Leucine', 'CUG':'Leucine', 'CCU':'Proline', 'CCC':'Proline', 'CCA':'Proline', 'CCG':'Proline', 'CAU':'Histidine', 'CAC':'Histidine', 'CAA':'Glutamine', 'CAG':'Glutamine', 'CGU':'Arginine', 'CGC':'Arginine', 'CGA':'Arginine', 'CGG':'Arginine', 'AUU':'Isoleucine', 'AUC':'Isoleucine', 'AUA':'Isoleucine', 'AUG':'Methionine', 'ACU':'Threonine', 'ACC':'Threonine', 'ACA':'Threonine', 'ACG':'Threonine', 'AUU':'Asparagine', 'AUC':'Asparagine', 'AAA':'Lysine', 'AAG':'Lysine', 'AGU':'Serine', 'AGC':'Serine', 'AGA':'Arginine', 'AGG':'Arginine', 'GUU':'Valine', 'GUC':'Valine', 'GUA':'Valine', 'GUG':'Valine', 'GCU':'Alanine', 'GCC':'Alanine', 'GCA':'Alanine', 'GCG':'Alanine', 'GAU':'Aspartic Acid', 'GAC':'Aspartic Acid', 'GAA':'Glutamic Acid', 'GAG':'Glutamic Acid', 'GGU':'Glycine', 'GGC':'Glycine', 'GGA':'Glycine', 'GGG':'Glycine'}
 aminoSLC = {'Isoleucine':'I', 'Leucine':'L', 'Valine':'V', 'Phenylalanine':'F', 'Methionine':'M', 'Cysteine':'C', 'Alanine':'A', 'Glycine':'G', 'Proline':'P', 'Threonine':'T', 'Serine':'S', 'Tyrosine':'Y', 'Tryptophan':'W', 'Glutamine':'Q', 'Asparagine':'N', 'Histidine':'H', 'Glutamic Acid':'E', 'Aspartic Acid':'D', 'Lysine':'K', 'Arginine':'R', 'STOP':'Stop', '_':'_'}
 nucleotides = ['A', 'C', 'G', 'T']
-nucleotidecount = {'A':0, 'C':0, 'G':0, 'T':0}
 compliment = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
 
 def basicanalysis(sequence):
@@ -13,6 +12,7 @@ def basicanalysis(sequence):
     length = len(sequence)
     print("BP count:", length, "nucleotides")
     #nucleotide count
+    nucleotidecount = {'A':0, 'C':0, 'G':0, 'T':0}
     for char in sequence:
         if char in nucleotides: #sometimes N is included if the base in unknown
             nucleotidecount[char] += 1
@@ -144,9 +144,28 @@ x= rnatemplate(sequence)
 print(reference)
 translation(x)
 '''
-data = open("PythonSampleDNA.csv", 'r')
-sampleDNA = data.readlines()
-output = open("PythonSampleOutput.csv", 'w')
+data = open("PythonSampleDNA.csv.csv", 'r') 
+sample = data.readlines()
+
+output = open("PythonSampleOutput.csv.csv", 'w')
+
+for seq in sample[0:9]:
+    seq = seq [:-8]
+    s = complimentarysequence(seq)
+    
+    #print(seq)
+    pass
+    
+    output.write(s)
+    output.write('\n')
+    '''
+    output.write(complimentarysequence(sequence))
+    x= rnatemplate(sequence)
+    output.write(x)
+    output.write(translation(x))
+    '''
+output.close()
+data.close()
 
 #plan: substitute with file reading and writing since the blast api didn't work out
 #create two files in my folder with the github stuff
